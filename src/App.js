b727838dcfd4e2da25fcaf93d101a35f1a8d7e25
Felipe Ferreira <felipesqf@gmail.com>
2021-02-06 13:43:24 +1100

    changes hash route

--- src/App.js
+++ src/App.js
@@ -6,20 +6,14 @@ import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import './App.css';
 
-import { createBrowserHistory } from "history";
-
-const history = createBrowserHistory();
-
 function App() {
   return (
-    <Router history={history}>
-      <Switch>
+    <HashRouter>
           <Route exact path="/" component={About} />
           <Route exact path="/experience" component={Experience} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
-      </Switch>
-    </Router>
+    </HashRouter>
   );
 }
 export default App;
